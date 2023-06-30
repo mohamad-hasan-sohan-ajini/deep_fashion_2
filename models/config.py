841369ad9eeb9cf618f1149b49b4b49b23adf4d0
mohamad-hasan-sohan-ajini <mohamad.hasan.sohan.ajini@gmail.com>
2023-06-30 16:10:24 +0330

    Add loss and criterion weight params

--- models/config.py
+++ models/config.py
@@ -14,3 +14,11 @@ class ModelConfig:
     max_objects: int = 10
     num_classes: int = 13 + 1
     num_keypoints: int = 294
+    # matcher parameters
+    bbox_matching_weight: float = 5.
+    class_matching_weight: float = 1.
+    keypoint_matching_weight: float = 1e-2
+    # loss function parameters
+    bbox_loss_weight: float = 5
+    class_loss_weight: float = 1
+    giou_loss_weight: float = 2
