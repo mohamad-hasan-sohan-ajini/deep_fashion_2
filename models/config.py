2885385615a94fa592e2f8a0935c5756e479aabf
mohamad-hasan-sohan-ajini <mohamad.hasan.sohan.ajini@gmail.com>
2023-06-30 19:56:33 +0330

    Add class0 weight

--- models/config.py
+++ models/config.py
@@ -22,6 +22,7 @@ class ModelConfig:
     bbox_loss_weight: float = 5
     class_loss_weight: float = 1
     giou_loss_weight: float = 2
+    class0_weight: float = 3e-2
     # optimizer and scheduler parameters
     feature_lr: float = 1e-4
     transformer_lr: float = 1e-5
