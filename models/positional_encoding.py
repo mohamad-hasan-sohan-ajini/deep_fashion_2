95dd162c3d308be7e72432f262f040d4fb7fef0d
aj <mohamad.hasan.sohan.ajini@gmail.com>
2026-02-16 00:22:34 +0000

    Format code

--- models/positional_encoding.py
+++ models/positional_encoding.py
@@ -8,11 +8,12 @@ from torch import Tensor, nn
 
 class PositionalEncoding2D(nn.Module):
     """Sinusoidal 2D Positional Encoding Base Class"""
+
     def __init__(
-            self,
-            d_model: int,
-            height: int,
-            width: int,
+        self,
+        d_model: int,
+        height: int,
+        width: int,
     ) -> None:
         """Initialize
 
@@ -25,39 +26,29 @@ class PositionalEncoding2D(nn.Module):
         pe = torch.zeros(d_model, height, width)
         d_model = int(d_model / 2)
         div_term = torch.exp(
-            torch.arange(0., d_model, 2) * -(math.log(10000.0) / d_model)
+            torch.arange(0.0, d_model, 2) * -(math.log(10000.0) / d_model)
         )
-        pos_w = torch.arange(0., width).unsqueeze(1)
-        pos_h = torch.arange(0., height).unsqueeze(1)
+        pos_w = torch.arange(0.0, width).unsqueeze(1)
+        pos_h = torch.arange(0.0, height).unsqueeze(1)
         pe[0:d_model:2, :, :] = (
-            torch
-            .sin(pos_w * div_term)
+            torch.sin(pos_w * div_term)
             .transpose(0, 1)
             .unsqueeze(1)
             .repeat(1, height, 1)
         )
         pe[1:d_model:2, :, :] = (
-            torch
-            .cos(pos_w * div_term)
+            torch.cos(pos_w * div_term)
             .transpose(0, 1)
             .unsqueeze(1)
             .repeat(1, height, 1)
         )
         pe[d_model::2, :, :] = (
-            torch
-            .sin(pos_h * div_term)
-            .transpose(0, 1)
-            .unsqueeze(2)
-            .repeat(1, 1, width)
+            torch.sin(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
         )
-        pe[d_model + 1::2, :, :] = (
-            torch
-            .cos(pos_h * div_term)
-            .transpose(0, 1)
-            .unsqueeze(2)
-            .repeat(1, 1, width)
+        pe[d_model + 1 :: 2, :, :] = (
+            torch.cos(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
         )
-        self._pe = pe
+        self._pe = pe.view(d_model * 2, -1).permute(1, 0).contiguous()
 
     def forward(self, x: Tensor) -> Tensor:
         """Add positional encoding to the signal
@@ -72,30 +63,31 @@ class PositionalEncoding2D(nn.Module):
 
 class FixedPositionalEncoding2D(PositionalEncoding2D):
     """Sinusoidal 2D Positional Encoding"""
+
     def __init__(
-            self,
-            d_model: int,
-            height: int,
-            width: int,
+        self,
+        d_model: int,
+        height: int,
+        width: int,
     ) -> None:
         super().__init__(d_model, height, width)
-        self.register_buffer('pe', self._pe)
+        self.register_buffer("pe", self._pe)
 
 
 class LearnablePositionalEncoding2D(PositionalEncoding2D):
     """Sinusoidal 2D Positional Encoding"""
 
     def __init__(
-            self,
-            d_model: int,
-            height: int,
-            width: int,
+        self,
+        d_model: int,
+        height: int,
+        width: int,
     ) -> None:
         super().__init__(d_model, height, width)
-        self.register_parameter('pe', nn.Parameter(self._pe))
+        self.register_parameter("pe", nn.Parameter(self._pe))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     d_model = 128
     height, width = 32, 32
     fix_pe2d = FixedPositionalEncoding2D(d_model, height, width)
